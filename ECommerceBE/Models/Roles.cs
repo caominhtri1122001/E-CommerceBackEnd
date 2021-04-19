9d4982af18750047ffa82550ff546c28648a4a78
Cao Minh Tri <caominhtri112201@gmail.com>
2021-04-19 16:55:04 +0700

    Base

---
+++ ECommerceBE/Models/Roles.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ECommerceBE.Models
+{
+    public class Roles
+    {
+        int roleID { get; set; }
+        string roleName { get; set; }
+    }
+    public class BaseRespone
+    {
+        public bool Success { get; set; }
+        public Object Data { get; set; }
+    }
+}
---
+++ ECommerceBE/Models/TodoContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace ECommerceBE.Models
+{
+    public class TodoContext: DbContext
+    {
+        public TodoContext(DbContextOptions<TodoContext> options)
+           : base(options)
+        {
+        }
+
+        public DbSet<TodoItem> TodoItems { get; set; }
+    }
+}
