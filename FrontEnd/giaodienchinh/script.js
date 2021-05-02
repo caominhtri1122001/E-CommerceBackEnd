34be8af69b6d92d2cc08b0ab8be46b829b0658f9
truongthimyduyen27042001 <truongthimyduyen27042001@gmail.com>
2021-05-02 13:19:19 +0700

    update phan dang nhap dang xuat

---
+++ FrontEnd/giaodienchinh/script.js
@@ -0,0 +1,8 @@
+console.log("####   ")
+console.log("truong thi my duyen")
+let log=document.querySelector(".auth-form")
+let model=document.querySelector(".modal")
+
+function openLogInOut(){
+  alert("@@@@")
+}
