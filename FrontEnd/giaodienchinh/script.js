dd00b7decb9ba9dcb57ff73d529621ecd1ce7408
truongthimyduyen27042001 <truongthimyduyen27042001@gmail.com>
2021-05-03 12:11:26 +0700

    update giao diện register

--- FrontEnd/giaodienchinh/script.js
+++ FrontEnd/giaodienchinh/script.js
@@ -1,8 +1,24 @@
 console.log("####   ")
-console.log("truong thi my duyen")
-let log=document.querySelector(".auth-form")
+console.log("dunei truongn")
+let log=document.getElementById("auth-form")
 let model=document.querySelector(".modal")
-
+let container=document.querySelector(".app")
+let header=document.getElementById("header")
+let content=document.getElementById("app__container")
+//     background-color: #8e8e91;
+// opacity: 0.8;
+function shadowIndex(){
+  container.style.opacity="0.5"
+  content.style.backgroundColor="#8e8e91"
+  content.style.opacity="0.8"
+  
+}
+function offshadowIndex(){
+  container.style.opacity="1"
+  content.style.backgroundColor="#f5f5f5"
+  content.style.opacity="1"
+}
 function openLogInOut(){
-  alert("@@@@")
+  shadowIndex()
+  log.style.display="block"
 }
