0cd1450cea1b993d03952061940f680ffd9b67a6
truongthimyduyen27042001 <truongthimyduyen27042001@gmail.com>
2021-05-04 16:43:07 +0700

    done xong js register + login

--- FrontEnd/giaodienchinh/script.js
+++ FrontEnd/giaodienchinh/script.js
@@ -1,24 +1,35 @@
-console.log("####   ")
-console.log("dunei truongn")
-let log=document.getElementById("auth-form")
-let model=document.querySelector(".modal")
-let container=document.querySelector(".app")
-let header=document.getElementById("header")
-let content=document.getElementById("app__container")
-//     background-color: #8e8e91;
-// opacity: 0.8;
-function shadowIndex(){
-  container.style.opacity="0.5"
-  content.style.backgroundColor="#8e8e91"
-  content.style.opacity="0.8"
-  
+var modal = document.getElementById("myModal");
+var modal2=document.getElementById("myModal2")
+
+// Get the button that opens the modal
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks the button, open the modal 
+
+// When the user clicks on <span> (x), close the modal
+function openRegister(){
+    modal.style.display = "block";
 }
-function offshadowIndex(){
-  container.style.opacity="1"
-  content.style.backgroundColor="#f5f5f5"
-  content.style.opacity="1"
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal||event.target==modal2) {
+    modal.style.display = "none";
+    modal2.style.display = "none";
+  }
 }
+
+//Đăng nhập
 function openLogInOut(){
-  shadowIndex()
-  log.style.display="block"
+  modal2.style.display = "block";
+}
+//Thực hiện hàm khi bấm chỗ đăng nhập trong form sẵn
+function changeToLogin(){
+  modal.style.display = "none";
+  modal2.style.display = "block";
+}
+function changeToRegister(){
+  modal.style.display = "block";
+  modal2.style.display = "none";
 }
--- FrontEnd/giaodienchinh/testscript.js
+++ FrontEnd/giaodienchinh/testscript.js
@@ -1,7 +1,7 @@
 var modal = document.getElementById("myModal");
+var modal2=document.getElementById("myModal2")
 
 // Get the button that opens the modal
-console.log("truong duyen")
 
 // Get the <span> element that closes the modal
 var span = document.getElementsByClassName("close")[0];
@@ -9,12 +9,27 @@ var span = document.getElementsByClassName("close")[0];
 // When the user clicks the button, open the modal 
 
 // When the user clicks on <span> (x), close the modal
-function openLogInOut(){
+function openRegister(){
     modal.style.display = "block";
 }
 // When the user clicks anywhere outside of the modal, close it
 window.onclick = function(event) {
-  if (event.target == modal) {
+  if (event.target == modal||event.target==modal2) {
     modal.style.display = "none";
+    modal2.style.display = "none";
   }
+}
+
+//Đăng nhập
+function openLogInOut(){
+  modal2.style.display = "block";
+}
+//Thực hiện hàm khi bấm chỗ đăng nhập trong form sẵn
+function changeToLogin(){
+  modal.style.display = "none";
+  modal2.style.display = "block";
+}
+function changeToRegister(){
+  modal.style.display = "block";
+  modal2.style.display = "none";
 }
