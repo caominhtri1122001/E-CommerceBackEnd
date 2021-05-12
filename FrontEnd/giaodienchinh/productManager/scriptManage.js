81271f2fdf464dd2df443c8028ac7b6f349780d4
truongthimyduyen27042001 <truongthimyduyen27042001@gmail.com>
2021-05-12 14:06:36 +0700

    vu duong la dua lam tiep :v

---
+++ FrontEnd/giaodienchinh/productManager/scriptManage.js
@@ -0,0 +1,69 @@
+// Load dữ liệu cho thàng table 
+console.log("truong my duyen")
+
+function requestDataListProduct(url) {
+    $.ajax({
+        url: url,
+        data: null,
+        cache: false,
+        type: "GET",
+        success: function (response) {
+            if (response.success) {
+                console.log("thanh cong roi ne")
+                loadData(response.data)
+            }
+            else {
+                alert(response.message)
+            }
+        },
+        error: function (xhr) {
+
+        }
+    });
+}
+
+var goToPage = function (id) {
+    window.location.href = "/detailtest.html?productId=" + id;
+}
+
+
+var loadData = function (proudcts) {
+    for(var i=proudcts.length-1;i>=0;i--){
+
+        var productHtml = `
+        <div class="row list product">
+                        <div class="cell"  data-title="Name">
+                             ${proudcts[i].proID}
+                            </div>
+                            <div class="cell" id="proName" data-title="Age">
+                              ${proudcts[i].proName}
+                            </div>
+                            <div class="cell hinhanh" data-title="Occupation">
+                              <img src="${proudcts[i].proLinkPicture}" alt="">
+                            </div>
+                            <div class="cell" data-title="Location">
+                             ${proudcts[i].proBrand}
+                            </div>
+                            <div class="cell" data-title="numberSold">
+                            ${proudcts[i].numberOfSold}
+                            </div>
+                            <div class="cell func edit" data-title="edit">
+                                <i class="fas fa-edit"></i>
+                            </div>
+                            <div class="cell func" data-title="delete">
+                                <i class="fas fa-trash-alt"></i>
+                           </div>
+                            <div class="cell func" data-title="addmore">
+                                <i class="far fa-plus-square"></i>
+                            </div>
+                        </div>
+                          
+    `
+        $("#product-list-row").append(productHtml)
+    }
+}
+$(document).ready(function () {
+    console.log("ready!");
+    requestDataListProduct("http://localhost:37504/api/Product");
+});
+
